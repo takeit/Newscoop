2644c6853decfed014376cbd029f10a181f56c20
Rafał Muszyński <rmuszynski1@gmail.com>
2015-03-03 10:05:30 +0100

    CS-5700 - Playlist is not showing correct section for multi-language issues
    fix pagination css
    
    fix new playlist save
    
    added validation messages to inputs

--- newscoop/src/Newscoop/GimmeBundle/Controller/ArticlesListController.php
+++ newscoop/src/Newscoop/GimmeBundle/Controller/ArticlesListController.php
@@ -377,7 +377,7 @@ class ArticlesListController extends FOSRestController
                     );
                 } catch (\Exception $e) {
                     $actionsResults[$actionKey] = array(
-                        'object' => $e,
+                        'object' => $e->getMessage(),
                         'method' => $method,
                         'header' => $header,
                     );
@@ -452,8 +452,7 @@ class ArticlesListController extends FOSRestController
      *         404="Returned when resource not found"
      *     },
      *     parameters={
-     *         {"name"="id", "dataType"="integer", "required"=true, "description"="Playlist id"},
-     *         {"name"="access_token", "dataType"="string", "required"=false, "description"="Access token"}
+     *         {"name"="id", "dataType"="integer", "required"=true, "description"="Playlist id"}
      *     },
      *     input="\Newscoop\GimmeBundle\Form\Type\PlaylistType"
      * )
