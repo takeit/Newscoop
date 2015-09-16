1285d479d9450a544aa663f7b7a7ab32acff2416
Rafał Muszyński <rmuszynski1@gmail.com>
2015-09-16 16:41:36 +0200

    integrate updater bundle
    update composer dependencies, fixed updater routing file

--- newscoop/application/AppKernel.php
+++ newscoop/application/AppKernel.php
@@ -1,11 +1,10 @@
 <?php
+
 /**
- * @package Newscoop\Gimme
  * @author Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
  * @copyright 2012 Sourcefabric o.p.s.
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
-
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -20,7 +19,8 @@ class AppKernel extends Kernel
             if (!empty($timeZone)) {
                 date_default_timezone_set($timeZone);
             }
-        } catch (\Exception $e) { }
+        } catch (\Exception $e) {
+        }
 
         // Set temp dir for Newscoop to project cache directory
         putenv(sprintf('TMPDIR=%s/../cache', __DIR__));
@@ -28,7 +28,7 @@ class AppKernel extends Kernel
 
     public function registerBundles()
     {
-        $bundles =  array(
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -53,7 +53,8 @@ class AppKernel extends Kernel
             new Newscoop\ArticlesBundle\NewscoopArticlesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
-            new Newscoop\PluginGeneratorBundle\NewscoopPluginGeneratorBundle()
+            new Newscoop\PluginGeneratorBundle\NewscoopPluginGeneratorBundle(),
+            new SWP\UpdaterBundle\SWPUpdaterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -63,7 +64,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
         }
 
-        $cachedPlugins = __DIR__ . '/../plugins/available_plugins.json';
+        $cachedPlugins = __DIR__.'/../plugins/available_plugins.json';
         if (file_exists($cachedPlugins)) {
             $plugins = json_decode(file_get_contents($cachedPlugins));
             foreach ($plugins as $plugin) {
@@ -81,12 +82,12 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return $this->truepath($this->rootDir . '/../cache/' . $this->environment);
+        return $this->truepath($this->rootDir.'/../cache/'.$this->environment);
     }
 
     public function getLogDir()
     {
-        return $this->truepath($this->rootDir . '/../log');
+        return $this->truepath($this->rootDir.'/../log');
     }
 
     /**
@@ -103,33 +104,41 @@ class AppKernel extends Kernel
 
     /**
      * This function is to replace PHP's extremely buggy realpath().
+     *
      * @param string The original path, can be relative etc.
+     *
      * @return string The resolved path, it might not exist.
      */
     public function truepath($path)
     {
         // whether $path is unix or not
-        $unipath=strlen($path)==0 || $path{0}!='/';
+        $unipath = strlen($path) == 0 || $path{0}
+        != '/';
         // attempts to detect if path is relative in which case, add cwd
-        if(strpos($path,':')===false && $unipath)
-            $path=getcwd().DIRECTORY_SEPARATOR.$path;
+        if (strpos($path, ':') === false && $unipath) {
+            $path = getcwd().DIRECTORY_SEPARATOR.$path;
+        }
         // resolve path parts (single dot, double dot and double delimiters)
         $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
         $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
         $absolutes = array();
         foreach ($parts as $part) {
-            if ('.'  == $part) continue;
+            if ('.'  == $part) {
+                continue;
+            }
             if ('..' == $part) {
                 array_pop($absolutes);
             } else {
                 $absolutes[] = $part;
             }
         }
-        $path=implode(DIRECTORY_SEPARATOR, $absolutes);
+        $path = implode(DIRECTORY_SEPARATOR, $absolutes);
         // resolve any symlinks
-        if(file_exists($path) && linkinfo($path)>0)$path=readlink($path);
+        if (file_exists($path) && linkinfo($path) > 0) {
+            $path = readlink($path);
+        }
         // put initial separator that could have been lost
-        $path=!$unipath ? '/'.$path : $path;
+        $path = !$unipath ? '/'.$path : $path;
 
         return $path;
     }
